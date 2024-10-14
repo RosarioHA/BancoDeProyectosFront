ade64df4efe282db7fee4337263b64ca6c1bc665
Veronica Caro <vecarope@Daniels-MacBook-Pro.local>
2024-10-14 09:37:27 -0300

    vistas gestion de usuarios

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,50 +1,35 @@
 import { useState, useContext } from 'react';
 import { apiBancoProyecto } from '../services/bancoproyecto.api';
-import { generateCodeVerifier, generateCodeChallenge, calculateHash, encrypt, decryptCodeVerifier } from '../config/authUtils';
+import { generateCodeVerifier, generateCodeChallenge, encrypt, decryptCodeVerifier } from '../config/authUtils';
 import { AuthContext } from '../context/AuthContext';
-import axios from 'axios';
 
 
 export const useLogin = () => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
     const { login: globalLogin, userData } = useContext(AuthContext);
     
     const loginWithKeycloak = async () => {
         setLoading(true);
         try {
-            // Generar code_verifier y code_challenge aquí
             const codeVerifier = generateCodeVerifier();
             const codeChallenge = await generateCodeChallenge(codeVerifier);
-            const encryptedCodeVerifier = encrypt(codeVerifier); 
-                    
+            const encryptedCodeVerifier = encrypt(codeVerifier);
 
-            // Guardar el codeVerifier en algún lugar para su uso posterior
             localStorage.setItem('codeVerifier', codeVerifier);
-            // console.log("Se guarda el code Verifier: ", codeVerifier);
-
-            // Construir la URL de redirección manualmente con los parámetros necesarios
             const redirectUri = import.meta.env.VITE_KEYCLOAK_REDIRECT_URI;
             const clientId = import.meta.env.VITE_KEYCLOAK_RESOURCE;
             const keycloakAuthUrl = import.meta.env.VITE_KEYCLOAK_AUTH_URL;
 
-            const state = encodeURIComponent(encryptedCodeVerifier); // Codificar el hash para la URL
-            // console.log("Se encripta el code Verifier: ", encryptedCodeVerifier);
-
+            const state = encodeURIComponent(encryptedCodeVerifier);
             const authUrl = `${keycloakAuthUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid&code_challenge=${codeChallenge}&code_challenge_method=S256&state=${state}`;
-            // console.log("URL de autenticación: ", authUrl);
 
-
-            // Redirigir al usuario a Keycloak para el login
             window.location.href = authUrl;
-
             localStorage.setItem('isKeycloak', 'true');
-
         } catch (initError) {
-            setError(`Error initializing login process: ${initError}`);
             console.error('Error during login initialization:', initError);
+            setError(`Error initializing login process: ${initError.message || 'Unknown error'}`);
         } finally {
             setLoading(false);
         }
@@ -53,88 +38,65 @@ export const useLogin = () => {
     const handleAuthentication = async (code, state) => {
         setLoading(true);
         try {
-            // console.log("handleAuthentication recibe el code y state")
-            // console.log("code :", code);
             const codeVerifier = decryptCodeVerifier(state);
-            // console.log("codeVerifier :", codeVerifier);
-   
-            // Intercambiar el código por un token utilizando el codeVerifier
             const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
             const response = await apiBancoProyecto.post(`${apiUrl}/callback/`, {
                 code,
-                codeVerifier  // Enviar el codeVerifier recibido de Keycloak
+                codeVerifier
             });
 
-            // console.log("se envía el code :", code, " y el codeVerifier: ", codeVerifier)
-    
-            if (response.data && response.data.access_token && response.data.refresh_token) {
-                localStorage.setItem('userToken', response.data.access_token);
+            if (response.data && response.data.access_token) {
+                const { access_token, refresh_token, expires_in } = response.data;
+                localStorage.setItem('userToken', access_token);
                 localStorage.setItem('userData', JSON.stringify(response.data.user));
-                localStorage.setItem('refreshToken', response.data.refresh_token);
-                const expiresAt = new Date().getTime() + (response.data.expires_in * 1000);
-                localStorage.setItem('tokenExpiry', expiresAt.toString());
+                localStorage.setItem('refreshToken', refresh_token);
+                localStorage.setItem('tokenExpiry', Date.now() + expires_in * 1000);
                 localStorage.setItem('authMethod', 'keycloak');
+
                 setData(response.data);
-                globalLogin(response.data.access_token, response.data.refresh_token, response.data.user);
-                // console.log("El backend devuelve los tokens necesarios")
+                globalLogin(access_token, refresh_token, response.data.user);
             } else {
-                throw new Error("La respuesta del servidor no incluye los tokens necesarios.");
+                throw new Error("The server response does not include the necessary tokens.");
             }
         } catch (error) {
             console.error('Error during the authentication process:', error);
-            setError(`Error during the authentication process: ${error.message || 'Unknown error'}`);
+            setError(`Error during authentication: ${error.response?.data?.detail || error.message || 'Unknown error'}`);
         } finally {
             setLoading(false);
         }
     };
 
-
-    // Función para refrescar el token si está próximo a expirar
-
     const isTokenExpired = () => {
         const expiry = localStorage.getItem('tokenExpiry');
-        if (!expiry) return true;
-        return Date.now() > parseInt(expiry);
+        return !expiry || Date.now() > parseInt(expiry);
     };
 
     const refreshAccessToken = async () => {
         setLoading(true);
         try {
-            // console.log("Refresh Access Token")
             if (isTokenExpired()) {
                 const refreshToken = localStorage.getItem('refreshToken');
                 if (!refreshToken) {
-                    console.error("Refresh token not found.");
-                    // Opcionalmente, redirigir al usuario al login
                     setError("Login required.");
-                    setLoading(false);
                     return;
                 }
 
-                const response = await axios.post('/refresh-token/', { refresh_token: refreshToken });
-
+                const response = await apiBancoProyecto.post('/refresh-token/', { refresh_token: refreshToken });
                 if (response.data && response.data.access_token) {
-                    // Actualizar el contexto y el almacenamiento local con el nuevo token de acceso y refresco
-                    const { access_token, refresh_token, expires_in } = response.data;
+                    const { access_token, refresh_token, expires_in } = response.data;    
                     localStorage.setItem('userToken', access_token);
-                    localStorage.setItem('tokenExpiry', Date.now() + expires_in * 1000); // Almacenar la nueva caducidad del token
+                    localStorage.setItem('tokenExpiry', Date.now() + expires_in * 1000);
                     if (refresh_token) {
-                        localStorage.setItem('refreshToken', refresh_token); // Si se devuelve un nuevo refresh token
+                        localStorage.setItem('refreshToken', refresh_token);
                     }
-
-                    // console.log("refresh acces token", access_token)
-                    // console.log("refresh refresh token", access_token)
-                    // console.log("refresh Expiry token", access_token)
-
-                    globalLogin(access_token, refresh_token, userData); // Asegúrate de que userData esté definido correctamente
+                    globalLogin(access_token, refresh_token, userData);
                 } else {
-                    console.error('Failed to refresh access token.');
-                    setError('Failed to refresh access token.');
+                    throw new Error('Failed to refresh access token.');
                 }
             }
         } catch (error) {
             console.error('Error refreshing access token:', error);
-            setError('Error refreshing access token.');
+            setError(`Error refreshing access token: ${error.response?.data?.detail || error.message || 'Unknown error'}`);
         } finally {
             setLoading(false);
         }
---
+++ src/views/Dashboard/admin/GestionUsuarios.jsx
@@ -0,0 +1,160 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useUsers } from "../../../hooks/usuarios/useUsers";
+import { useAuth } from '../../../context/AuthContext';
+import Buscador from '../../../components/Commons/barraDeBusqueda'; 
+
+const GestionUsuarios = () => {
+  const { users, pagination, setPagination, fetchUsers, metadata } = useUsers(); 
+  const { userData } = useAuth();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [allUsers, setAllUsers] = useState([]); // Estado para almacenar todos los usuarios
+  const [searchResults, setSearchResults] = useState([]);
+  const [searching, setSearching] = useState(false);
+  const navigate = useNavigate();
+  const usersPerPage = 10;
+
+  console.log(userData)
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers, pagination]);
+
+  useEffect(() => {
+    // Almacenar todos los usuarios cuando se recuperan
+    setAllUsers(users);
+  }, [users]);
+
+  useEffect(() => {
+    const normalizedTerm = searchTerm.trim().toLowerCase();
+    const filteredUsers = allUsers.filter((user) => {
+      const userNameLower = user.full_name.toLowerCase();
+      return (
+        userNameLower.includes(normalizedTerm) ||
+        user.id.toString().includes(normalizedTerm)
+      );
+    });
+
+    // Actualiza searchResults con todos los usuarios filtrados
+    setSearchResults(filteredUsers);
+  }, [allUsers, searchTerm]); // Cambiar `users` a `allUsers`
+
+  const handleDetailsUser = (user) => {
+    navigate(`/dashboard/editar_usuario/${user.id}`, { state: { user } });
+  };
+
+  const handleSearch = (term) => {
+    const normalizedTerm = term.trim().toLowerCase();
+    setSearchTerm(normalizedTerm);
+    setSearching(!!normalizedTerm);
+  };
+
+  const totalPages = Math.ceil(metadata.count / usersPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setPagination(pageNumber);
+  };
+
+  const renderPaginationButtons = () => {
+    if (!pagination) {
+      return null;
+    }
+
+    return (
+      <div className="d-flex flex-column flex-md-row my-5">
+        <p className="text-sans-h5 mx-5 text-center">
+          {`${(pagination - 1) * usersPerPage + 1}- ${Math.min(pagination * usersPerPage, metadata.count)} de ${metadata.count} usuarios`}
+        </p>
+        <nav className="pagination-container mx-auto mx-md-0">
+          <ul className="pagination ms-md-5">
+            <li className={`page-item ${pagination === 1 ? 'disabled' : ''}`}>
+              <button className="custom-pagination-btn mx-3" onClick={() => handlePageChange(pagination - 1)} disabled={pagination === 1}>
+                &lt;
+              </button>
+            </li>
+            {Array.from({ length: totalPages }, (_, i) => (
+              <li key={i} className="page-item">
+                <button className={`custom-pagination-btn text-decoration-underline px-2 mx-2 ${pagination === i + 1 ? 'active' : ''}`} onClick={() => handlePageChange(i + 1)}>
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+            <li className={`page-item ${pagination === totalPages ? 'disabled' : ''}`}>
+              <button className="custom-pagination-btn mx-3" onClick={() => handlePageChange(pagination + 1)} disabled={pagination === totalPages}>
+                &gt;
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container col-10 col-xxl-11 mt-2">
+      <div className="text-sans-h2 mx-3">Administrar Usuarios</div>
+      <div className="d-flex flex-row px-4 mb-5 justify-content-between">
+        <div className="w-50 pl-2 text-sans-24 align-self-center">Todos los usuarios</div>
+        <div>
+          <Buscador
+            searchTerm={searchTerm}
+            onSearch={handleSearch}
+            isSearching={searching}
+            setIsSearching={setSearching}
+            placeholder="Escribe un nombre de usuario"
+          />
+        </div> 
+      </div>
+      <div>
+        <div className="row py-2 border-top">
+          <div className="col-1 mt-3">#</div>
+          <div className="col mt-3">
+            <p className="text-sans-b-gray">Nombre</p>
+          </div>
+          <div className="col mt-2">
+            <p className="text-sans-b-gray">Correo Electrónico</p>
+          </div>
+          <div className="col mt-2">
+            <button className="sort-estado-btn d-flex align-items-top mx-5">
+              <p className="text-sans-b-gray mt-1 mx-2">Tipo de usuario</p>
+              <i className="material-symbols-rounded ">filter_alt</i>
+            </button>
+          </div>
+          <div className="col mt-3">
+            <p className="text-sans-b-gray">Acción</p>
+          </div>
+          {searchTerm === '' ? (
+            users.map((user, index) => (
+              <div key={index} className={`row border-top ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}>
+                <div className="col-1 p-3">{(pagination - 1) * usersPerPage + index + 1}</div>
+                <div className="col p-3">{user.full_name}</div>
+                <div className="col p-3">{user.email}</div>
+                <div className="col p-3 mx-2 "><span className="badge-status-finish mx-1">{user.perfil}</span></div>
+                <div className="col p-3"><button className="btn-secundario-s px-3 py-1" onClick={() => handleDetailsUser(user)}><u>Ver usuario</u></button></div>
+              </div>
+            ))
+          ) : searchResults.length > 0 ? (
+            searchResults.map((user, index) => (
+              <div key={index} className={`row border-top ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}>
+                <div className="col-1 p-3">{index + 1}</div>
+                <div className="col p-3">{user.full_name}</div>
+                <div className="col p-3">{user.email}</div>
+                <div className="col p-3">{user.perfil}</div>
+                <div className="col p-3"><button className="action-btn px-3 py-1"  onClick={() => handleDetailsUser(user)}>Ver usuario</button></div>
+              </div>
+            ))
+          ) : (
+            <div>No se encontraron usuario.</div>
+          )}
+        </div>
+        {metadata.count > usersPerPage && (
+          <div className="pagination-container d-flex justify-content-center">
+            {renderPaginationButtons()}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default GestionUsuarios;
